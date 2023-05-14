2ea1c157d3a3639cd430d8eae22006de614da2e4
Prakash <pk428357@gmail.com>
2023-05-14 23:44:07 +0530

    student portal

--- component/BottomNav/index.js
+++ component/BottomNav/index.js
@@ -6,8 +6,11 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn'
 import { useState } from 'react';
 import Passcode from '../Passcode';
+import { useRouter } from 'next/router';
+import { signOut } from 'next-auth/react';
 
 function BottomNav() {
+  const router = useRouter()
   const [value, setValue] = useState(0);
   const [open, setOpen] = useState(false);
   const handleClick = () => {
@@ -24,7 +27,7 @@ function BottomNav() {
         }}
       >
         <BottomNavigationAction label="Get Attendance" icon={<RestoreIcon />} onClick={handleClick} />
-        <BottomNavigationAction label="Log out" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Log out" icon={<FavoriteIcon />} onClick={() => signOut({ redirect: false }).then((response) => { router.push(`${process.env.NEXT_PUBLIC_BASE_URL}`) })} />
       </BottomNavigation>
 
     </>
